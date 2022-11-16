4076206c93d0bf683417036be3053da1e237b4c9
salt-dev <swetha.chauhan0001@gmail.com>
2022-11-16 13:02:26 +0100

    adding test

--- src/test/java/com/demo/githubActions/GithubActionsDemoApplicationTests.java
+++ src/test/java/com/demo/githubActions/GithubActionsDemoApplicationTests.java
@@ -1,13 +1,37 @@
 package com.demo.githubActions;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class GithubActionsDemoApplicationTests {
 
+    static HttpClient client = HttpClient.newBuilder().build();
     @Test
-    void contextLoads() {
+    void shouldCheckTheApiText() {
+        try {
+            HttpRequest req = HttpRequest.newBuilder(URI.create("http://localhost:8080/greet")).build();
+            HttpResponse<String> response = client.send(req, ofString(StandardCharsets.UTF_8));
+            String body = response.body();
+            String expected = "Hello World";
+            assertEquals(expected, body);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+
+        }
     }
 
 }
